9a956e60a97725eba2020f731158582d90437613
brunoqcx <brunotavm@gmail.com>
2016-10-09 22:05:55 -0300

    detailing findParity a bit more

--- findParity.js
+++ findParity.js
@@ -1,3 +1,20 @@
+/*
+Description:
+
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns N.
+
+For example:
+
+[2, 4, 0, 100, 4, 11, 2602, 36]
+
+Should return: 11
+
+[160, 3, 1719, 19, 11, 13, -21]
+
+Should return: 160
+
+https://www.codewars.com/kata/find-the-parity-outlier/javascript  */
+
 var lista = [ 14311919,
   -126227671,
   -118251535,
