53d833c0b41a036292cf5068deba7342d27d3b9a
BrunoQcx <brunotavm@gmail.com>
2016-11-01 11:55:49 -0200

    convertStringCamel final submission

--- converStringCamel.js
+++ converStringCamel.js
@@ -15,16 +15,43 @@ https://www.codewars.com/kata/convert-string-to-camel-case/train/javascript
 
 function toCamelCase(str){
   //should run on string if it finds "-" || "_" should eliminate [i], UpperCase[i+1] and continue on [i+2]
-  for (var i = 0; i < str.length; i++) {
-    //console.log(str.charAt(i));
-    if (str.charAt(i)==="-" || str.charAt(i)==="_" ) {
-      //(str.charAt(i)) = (str.charAt(i)).toUpperCase()
-      console.log("opa");
+
+  var separator = function(str) {
+    var tempArr = str.split("-")
+    if (tempArr.length===1) {
+        tempArr = str.split("_")
     }
-  }
+
+      return tempArr
+    }
+
+  var upperCaser = function(arr){
+    var tempArr = arr
+    var upperLetter = ""
+    var restWord = ""
+    for (var i = 1; i < arr.length; i++) {
+      upperLetter = arr[i].charAt(0).toUpperCase()
+      restWord = arr[i].substring(1)
+      tempArr[i] = upperLetter+restWord
+    }
+
+      return tempArr
+    }
+
+  var combinator = function(arr) {
+    var newStr = ""
+    arr.forEach(item => newStr = newStr.concat(item))
+
+      return newStr
+    }
+
+
+  return combinator(upperCaser(separator(str)))
 }
 
 
 
+
+console.log(toCamelCase("the_stealth_warrior"));
 
 console.log(toCamelCase("The_Stealth_Warrior"));
