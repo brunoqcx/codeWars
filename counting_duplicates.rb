578f9fb15e974ece6dda12aea787fff4da8e1c36
BrunoQcx <brunotavm@gmail.com>
2017-10-13 00:34:30 -0300

    counting_duplicates

---
+++ counting_duplicates.rb
@@ -0,0 +1,20 @@
+# Count the number of Duplicates
+
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+# Example
+
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (bandB)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+# http://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/ruby
+
+def duplicate_count(text)
+  arr = text.downcase.split('')
+  arr = arr.select { |l| arr.count(l) > 1 }.uniq.count
+end
